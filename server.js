136586302331040a80b02ae99e8c00eaf8a3c9f3
amado4884 <amado4884@gmail.com>
2021-03-03 22:06:34 -0500

    Added Cors & Nodemon (#1)
    * added cors & nodemon
    
    * added computerId to db

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const port = process.env.PORT || 5000;
-const jsonServer = require('json-server')
-const auth = require('json-server-auth')
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
 
 const app = jsonServer.create();
-const router = jsonServer.router('db.json')
+const router = jsonServer.router('db.json');
 
+app.use(cors());
 
-app.use(auth)
-app.use(router)
-
-app.db = router.db
+app.db = router.db;
 //binding app to db
-
+app.use(auth);
 app.use(router);
 
 app.listen(port, () => console.log(`\n** Running on port ${port} **\t http://localhost:${port}/\n`));
